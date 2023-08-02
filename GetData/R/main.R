b75faedfb1fcfc23735ea0e77dd971c50c0670cf
MoonlightGreenCheese <125077697+MoonlightGreenCheese@users.noreply.github.com>
2023-08-02 17:36:30 +0100

    Update main.R
    Aug 2, 2023
    Now we are able to download data from all the branches under TCGA and render them into MultiAssayExperiments.
    On Windows 10/11 there will be some data unavailable, because Windows 10/11 has set a maximum path length limit. You should also set your destination directory as SHORT as possible.
    Also, make sure you have enough space (about 8G at least) for the downloading and extracting process.

--- GetData/R/main.R
+++ GetData/R/main.R
@@ -5,27 +5,80 @@ source("~/GetData/R/DownloadTCGA.R")
 # Set system-default parameter
 init()
 
-# Show the list of Firehose datasets
-getFirehoseDatasets()
+# Get the list of Firehose datasets
+firehose_ls <- getFirehoseDatasets()
 
-# Show the list of running dates
-getFirehoseRunningDates()
+# Get the latest running date
+firehose_dt <- getFirehoseRunningDates()[1]
 
-# Download BRCA, COAD, PAAD data
-BRCA_Data <- LoadData("BRCA", "D:/")
-COAD_Data <- LoadData("COAD", "D:/")
-PAAD_Data <- LoadData("PAAD", "D:/")
+expname_ls <- c(
+            "RNASeqGene",
+            "RNASeq2Gene",
+            "miRNASeqGene",
+            "RNASeq2GeneNorm",
+            "CNASNP",
+            "CNVSNP",
+            "CNASeq",
+            "CNACGH",
+            "Methylation",
+            "Mutation",
+            "mRNAArray",
+            "miRNAArray",
+            "RPPAArray",
+            "GISTIC")
 
-# Get Pheno data
-BRCA_pData <- GetPhenoData(BRCA_Data)
-COAD_pData <- GetPhenoData(COAD_Data)
-PAAD_pData <- GetPhenoData(PAAD_Data)
+# Download data from TCGA
+Tumour_TCGA <- list()
+for (dataset in firehose_ls){
+  # Get phenodata
+  tmp_pData <- GetPhenoData(dataset, "D://")
 
-# Get Experiment data
-BRCA_Experiment <- GetExpressionSet(BRCA_Data)
-COAD_Experiment <- GetExpressionSet(COAD_Data)
-PAAD_Experiment <- GetExpressionSet(PAAD_Data)
+  # Get ExperimentList
+  maplist <- list()
+  tmp_explist <- list()
+  tmp_map <- data.frame()
 
-colData(BRCA_Experiment)@rownames
+  for (exp in expname_ls){
+    tmp_exp <- LoadData(dataset, "D://", exp)
+    if(length(tmp_exp) != 0){
+      tmp_explist[exp] <- tmp_exp
+      if(exp == "GISTIC"){
+        for (val in colData(tmp_exp$AllByGene)@rownames) {
+          vectr <- c(substr(val, 1, 12), val)
+          tmp_map <- rbind(tmp_map,vectr)
+        }
+      }
+      else if((dataset == 'COADREAD' | dataset == 'COAD' | dataset == 'READ') & exp == 'RNASeq2GeneNorm'){
+        for (val in c(colData(tmp_exp$illuminaga)@rownames, colData(tmp_exp$illuminahiseq)@rownames)) {
+          vectr <- c(substr(val, 1, 12), val)
+          tmp_map <- rbind(tmp_map,vectr)
+        }
+      }
+      else if((dataset == 'GBM' | dataset == 'GBMLGG' | dataset == 'OV') & exp == 'CNACGH'){
+        for (val in c(colData(tmp_exp$CGH_hg_244a)@rownames, colData(tmp_exp$CGH_hg_415k_g4124a)@rownames)) {
+          vectr <- c(substr(val, 1, 12), val)
+          tmp_map <- rbind(tmp_map,vectr)
+        }
+      }
+      else{
+        for (val in colData(tmp_exp)@rownames) {
+          vectr <- c(substr(val, 1, 12), val)
+          tmp_map <- rbind(tmp_map,vectr)
+        }
+      }
+      colnames(tmp_map) <- c('primary', 'colname')
+      maplist[[exp]] <- tmp_map
+    }
 
-BRCA_MAE <- MultiAssayExperiment(list("RNASeq2GeneNorm" = BRCA_Experiment), colData = BRCA_pData)
+  }
+
+  ## Create a sampleMap
+  sampMap <- listToMap(maplist)
+
+  tmp_save <- MultiAssayExperiment(tmp_explist, colData = tmp_pData, sampleMap = sampMap)
+  Tumour_TCGA[[dataset]] <- tmp_save
+  save(tmp_save, file = paste0("Tumour_", dataset, "_TCGA.RData"))
+}
+
+# check the list of MultiAssayExperiments
+Tumour_TCGA
